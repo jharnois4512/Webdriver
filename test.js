314f785ba6868b116f3b53c72985f3c47b146fe7
jharnois4512 <36828732+jharnois4512@users.noreply.github.com>
2020-09-22 21:53:56 -0400

    added fuctionality to take user input for running tests

--- test.js
+++ test.js
@@ -2,6 +2,7 @@ const webdriver = require('selenium-webdriver')
 const chrome = require('selenium-webdriver/chrome')
 const fire = require('selenium-webdriver/firefox')
 const edge = require('selenium-webdriver/edge')
+const ie = require('selenium-webdriver/ie')
 const proxy = require('selenium-webdriver/proxy')
 const proxyAddr = "127.0.0.1:8888"
 const fs = require('fs')
@@ -16,51 +17,121 @@ function readIn(file){
   return new Buffer.from(fs.readFileSync(file)).toString().split("\r\n")
 }
 
-async function webDrive() {
-  /* Chrome driver */
+async function webDrive(input) {
+  var driver
+  // var act = driver.actions();
+  // await act.sendKeys(webdriver.Key.CONTROL + "t").sendKeys("cnn").perform();
   try{
-    // var opts = new chrome.Options()
-    // opts.addExtensions(encode("adblock.crx"), encode("ublock.crx"), encode("privBadger.crx"), encode("ghostery.crx"), encode("adblockPlus.crx"), encode("disconnect.crx"), encode("duckduckgoEss.crx"))
-    // var driver = new webdriver.Builder()
-    //             .withCapabilities(webdriver.Capabilities.chrome())
-    //             .setChromeOptions(opts)
-    //             .setProxy(proxy.manual({https: proxyAddr}))
-    //             .build();
-    // driver.findElement({css:"body"}).sendKeys('{CTRL}t')
-    // await driver.sendKeys('{CTRL} + t')
-    /* Firefox driver */
-    // var fireCap = new webdriver.Capabilities()
-    // var fireOpts = new fire.Options()
-    // fireOpts.addExtensions("adblock.xpi", "ublock.xpi", "privBadger.xpi", "ghostery.xpi", "adblockPlus.xpi", "duckduckgoEss.xpi")
-    // fireCap.setAcceptInsecureCerts(true)
-    // var driver = new webdriver.Builder()
-    //             .withCapabilities(fireCap)
-    //             .setFirefoxOptions(fireOpts)
-    //             .forBrowser("firefox")
-    //             .setProxy(proxy.manual({https: proxyAddr}))
-    //             // .setFirefoxOptions(fireOpts)
-    //             .build()
-
-    /* Edge driver */
-    var edgeOptions = new edge.Options()
-    edgeOptions.addExtensions(encode("adblock.crx"), encode("ublock.crx"), encode("privBadger.crx"), encode("ghostery.crx"), encode("adblockPlus.crx"), encode("disconnect.crx"), encode("duckduckgoEss.crx"))
-    var driver = new webdriver.Builder()
-                .forBrowser("MicrosoftEdge")
-                .setEdgeOptions(edgeOptions)
-                .setProxy(proxy.manual({https: proxyAddr}))
-                .build()
-
-    ;(await driver).get("https://www.google.com")
-    /* Opera Driver */
-    
+    if(input.includes("c")){
+      var opts = new chrome.Options()
+      if(input.includes(1)){
+        opts.addExtensions(encode("adblock.crx"))
+      }
+      if(input.includes(2)){
+        opts.addExtensions(encode("privBadger.crx"))
+      }
+      if(input.includes(3)){
+        opts.addExtensions(encode("ublock.crx"))
+      }
+      if(input.includes(4)){
+        opts.addExtensions(encode("ghostery.crx"))
+      }
+      if(input.includes(5)){
+        opts.addExtensions(encode("adblockPlus.crx"))
+      }
+      if(input.includes(6)){
+        opts.addExtensions(encode("disconnect.crx"))
+      }
+      if(input.includes(7)){
+        opts.addExtensions(encode("duckduckgoEss.crx"))
+      }
+      driver = new webdriver.Builder()
+                  .withCapabilities(webdriver.Capabilities.chrome())
+                  .setChromeOptions(opts)
+                  .setProxy(proxy.manual({https: proxyAddr}))
+                  .build();
+    }
+    if(input.includes("f")){
+      var fireCap = new webdriver.Capabilities()
+      var fireOpts = new fire.Options()
+      if(input.includes(1)){
+        fireOpts.addExtensions("adblock.crx")
+      }
+      if(input.includes(2)){
+        fireOpts.addExtensions("privBadger.crx")
+      }
+      if(input.includes(3)){
+        fireOpts.addExtensions("ublock.crx")
+      }
+      if(input.includes(4)){
+        fireOpts.addExtensions("ghostery.crx")
+      }
+      if(input.includes(5)){
+        fireOpts.addExtensions("adblockPlus.crx")
+      }
+      if(input.includes(6)){
+        fireOpts.addExtensions("disconnect.crx")
+      }
+      if(input.includes(7)){
+        fireOpts.addExtensions("duckduckgoEss.crx")
+      }
+      fireCap.setAcceptInsecureCerts(true)
+      driver = new webdriver.Builder()
+                  .withCapabilities(fireCap)
+                  .setFirefoxOptions(fireOpts)
+                  .forBrowser("firefox")
+                  .setProxy(proxy.manual({https: proxyAddr}))
+                  .setFirefoxOptions(fireOpts)
+                  .build()
+    }
+    if(input.includes("e")){
+      var edgeOptions = new edge.Options()
+      if(input.includes(1)){
+        opts.addExtensions(encode("adblock.crx"))
+      }
+      if(input.includes(2)){
+        opts.addExtensions(encode("privBadger.crx"))
+      }
+      if(input.includes(3)){
+        opts.addExtensions(encode("ublock.crx"))
+      }
+      if(input.includes(4)){
+        opts.addExtensions(encode("ghostery.crx"))
+      }
+      if(input.includes(5)){
+        opts.addExtensions(encode("adblockPlus.crx"))
+      }
+      if(input.includes(6)){
+        opts.addExtensions(encode("disconnect.crx"))
+      }
+      if(input.includes(7)){
+        opts.addExtensions(encode("duckduckgoEss.crx"))
+      }
+      driver = new webdriver.Builder()
+                  .forBrowser("MicrosoftEdge")
+                  .setEdgeOptions(edgeOptions)
+                  .setProxy(proxy.manual({https: proxyAddr}))
+                  .build()
+    }
+    if(input.includes("i")){
+      driver = new webdriver.Builder()
+      .forBrowser("ie")
+      .setProxy(proxy.manual({https: proxyAddr}))
+      .build()
+      ;(await driver).manage().setTimeouts({implicit: 90})
+    }
     /* sites to start */
-    // var sites = readIn("topSites.txt")
-    // for(var i = 0; i < sites.length; i++){
-    //   await driver.get(sites[i])
-    // }
+    var sites = readIn("topSites.txt")
+    for(var i = 0; i < sites.length; i++){
+      await driver.get(sites[i])
+    }
+    (await driver).close()
+    ;(await driver).quit()
   }
   catch(e){
     console.log(e)
   }
 }
-webDrive()
+
+webDrive(["c", 2])
+
